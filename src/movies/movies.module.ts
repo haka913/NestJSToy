1938ae84ce25c335739c984f666026927a152bb3
paul <ace95913@gmail.com>
2022-03-01 20:15:10 +0900

    make REST API

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -4,6 +4,7 @@ import { MoviesService } from './movies.service';
 
 @Module({
     controllers: [MoviesController],
+    // DI(dependency injection)
     providers: [MoviesService]
 })
 export class MoviesModule {}
